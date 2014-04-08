bf7e42d32b992c54a5e57b333b218af3baeadec7
Kristiyan Yordanov <kristiyanjordanov@gmail.com>
2014-04-08 17:32:17 +0200

    removed autowiring PlayerService.

--- src/main/java/kris/java/config/HomeController.java
+++ src/main/java/kris/java/config/HomeController.java
@@ -21,8 +21,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-	@Autowired
-	PlayerService playerService;
 
 	/**
 	 * Simply selects the home view to render by returning its name.
